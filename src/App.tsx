c7f33c094fbaff5b7a56637d60096f6b1c45093b
Singh8700 <rohit.si8700@gmail.com>
2023-06-30 21:51:36 +0530

    first-commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const API_KEY = '46f80a02ecae410460d59960ded6e1c6'; // Replace with your OpenWeatherMap API key
+const API_KEY = 'd82e09b293bf75b075736057842eb627'; // Replace with your OpenWeatherMap API key
 
 const App = () => {
   const [weatherData, setWeatherData] = useState('')
@@ -23,10 +23,10 @@ const App = () => {
       const response = await
       axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`)
       const res = await response.data
-      //console.log("weather adat",res)
+      //console.log("weather adat",response)
       setWeatherData(res);
     } catch (error) {
-      console.error('Error fetching weather data:', error);
+      //console.error('Error fetching weather data:', error);
       //alert('Error fetching weather data:', error)
     }
   }
@@ -50,11 +50,14 @@ const App = () => {
       </form>
       {weatherData && (
         <div>
-          <h2>{weatherData.name}</h2>
+        <img className="imgs"
+        src={`https://openweathermap.org/img/wn/${weatherData.weather[0].icon}@4x.png`}/>
+          <h2 style={{textAlign:"center",fontSize:'2rem',marginTop:"0rem"}}>{weatherData.name}</h2>
           <p>Temperature: {weatherData.main.temp}°C</p>
           <p>Humidity: {weatherData.main.humidity}%</p>
           <p>Wind Speed: {weatherData.wind.speed} m/s</p>
           <p>Description: {weatherData.weather[0].description}</p>
+          
         </div>
       )}
     </div>
